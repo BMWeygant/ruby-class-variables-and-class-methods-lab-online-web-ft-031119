43c1993866d57f9192d9df8765a52b966f036dd7
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-03-20 18:31:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,10 +32,10 @@ end
 def self.genre_count
   genre_count = {}
   @@genres.each do |genre|
-    if genre_count
-      genre_count += 1
+    if genre_count[genre]
+      genre_count[genre] += 1
     else
-      genre_count = 1
+      genre_count[genre] = 1
     end
   end
   genre_count
